f9c1d13dd4584af2af9fc2071498d15d783ba49b
jeanpierre <jeanpinie13998ef@linkiafp.online>
2024-12-11 09:47:04 +0100

    CONTACTOS

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Contactos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(18, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(348, 24);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(19, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(348, 24);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 24);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "agregar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(290, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 21);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "actualizar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(310, 291);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 21);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "borrar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(308, 417);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 21);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "salir";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(19, 264);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(285, 147);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Contactos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(18, 171);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(217, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(18, 222);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(217, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Contacto Actual";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Contacto Nuevo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Contactos
+{
+    public partial class Form1 : Form
+    {
+        // Lista para almacenar los contactos
+        private List<string> contactos = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Método para buscar un contacto por nombre (primer botón: Buscar)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombreBuscado = textBox1.Text.Trim();
+            if (!string.IsNullOrEmpty(nombreBuscado))
+            {
+                var resultado = contactos.Find(c => c.Contains(nombreBuscado));
+                if (resultado != null)
+                {
+                    MessageBox.Show($"Contacto encontrado: {resultado}");
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró el contacto.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese un nombre para buscar.");
+            }
+        }
+
+        // Método para agregar un nuevo contacto (segundo botón: Agregar)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string nuevoContacto = textBox2.Text.Trim();
+            if (!string.IsNullOrEmpty(nuevoContacto))
+            {
+                contactos.Add(nuevoContacto);
+                ActualizarListaContactos();
+                textBox2.Clear(); // Limpiar el TextBox
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese un nombre para agregar un contacto.");
+            }
+        }
+
+        // Método para actualizar un contacto (tercer botón: Actualizar)
+        // Método para actualizar un contacto (tercer botón: Actualizar)
+        // Método para borrar un contacto seleccionado de la ListBox (tercer botón: Borrar)
+        // Método para actualizar un contacto seleccionado en la ListBox
+        // Método para actualizar un contacto (tercer botón: Actualizar)
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string nombreViejo = textBox3.Text.Trim(); // Obtener el contacto actual desde textBox3
+            string nombreNuevo = textBox4.Text.Trim(); // Obtener el nuevo contacto desde textBox4
+
+            if (!string.IsNullOrEmpty(nombreViejo) && !string.IsNullOrEmpty(nombreNuevo))
+            {
+                // Buscar el índice del contacto actual en la lista
+                int index = contactos.FindIndex(c => c.Equals(nombreViejo, StringComparison.OrdinalIgnoreCase));
+                if (index != -1)
+                {
+                    // Actualizar el contacto en la lista
+                    contactos[index] = nombreNuevo;
+                    ActualizarListaContactos();
+                    textBox3.Clear(); // Limpiar el campo de contacto actual
+                    textBox4.Clear(); // Limpiar el campo de nuevo contacto
+                    MessageBox.Show("Contacto actualizado correctamente.");
+                }
+                else
+                {
+                    MessageBox.Show("El contacto a actualizar no existe.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese ambos campos: el contacto actual y el nuevo.");
+            }
+        }
+
+
+
+
+        // Método para borrar un contacto (cuarto botón: Borrar)
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string nombreABorrar = listBox1.Text.Trim();
+            if (!string.IsNullOrEmpty(nombreABorrar))
+            {
+                var contactoABorrar = contactos.Find(c => c.Equals(nombreABorrar, StringComparison.OrdinalIgnoreCase));
+                if (contactoABorrar != null)
+                {
+                    contactos.Remove(contactoABorrar);
+                    ActualizarListaContactos();
+                    textBox1.Clear();
+                    MessageBox.Show("Contacto eliminado correctamente.");
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró el contacto para eliminar.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese un nombre para eliminar.");
+            }
+        }
+
+        // Método para actualizar el ListBox con la lista de contactos
+        private void ActualizarListaContactos()
+        {
+            listBox1.Items.Clear();
+            foreach (var contacto in contactos)
+            {
+                listBox1.Items.Add(contacto);
+            }
+        }
+
+        // Método para salir del programa (quinto botón: Salir)
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Evento para buscar un contacto mientras se escribe (opcional)
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string nombreBuscado = textBox1.Text.Trim();
+            // Lógica para buscar el contacto mientras se escribe
+            if (!string.IsNullOrEmpty(nombreBuscado))
+            {
+                var resultado = contactos.Find(c => c.Contains(nombreBuscado));
+                if (resultado != null)
+                {
+                    MessageBox.Show($"Contacto encontrado: {resultado}");
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró el contacto.");
+                }
+            }
+        }
+
+        // Evento para manejar cambios en textBox2 (puedes agregar lógica si lo necesitas)
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Lógica que se ejecutará cuando el texto cambie en textBox2
+            // Por ejemplo, podrías validar el texto o hacer algo dinámico
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
